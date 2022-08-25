9d6e17fba0c045ce3392edd1923b559a8e206a48
Travers La Ville <traverslaville@traverss-mbp.lan>
2022-08-24 23:32:02 -0500

    Fixed Pylance error in app.y

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
-   .update_one(query_parameter, {"$set": data}, options)
+   
 # Run Flask
 if __name__ == "__main__":
    app.run()
